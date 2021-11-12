6046c841cc8766ce92d419c1f8fc6b9a46f470c2
IgnacioSoto <isoto@columbushs.com>
2021-11-12 08:43:42 -0500

    save

--- unit6/src/com/columbushs/unit6/Unit6Main.java
+++ unit6/src/com/columbushs/unit6/Unit6Main.java
@@ -15,9 +15,29 @@ public class Unit6Main {
 		Unit6_3EnhancedForLoop enhancedFor = new Unit6_3EnhancedForLoop();
 		//enhancedFor.enhancedForLoopsInts();
 		//enhancedFor.enhancedForLoopsStrings();
-		enhancedFor.enhancedForLoopsObj();
+		//enhancedFor.enhancedForLoopsObj();
 		
 		
+		Unit6_4ArrayAlgorithms arrayAlg = new Unit6_4ArrayAlgorithms();
+		double [] values = {12.5,67.2,67.3,45,32,19};
+		//System.out.println(arrayAlg.findMax(values));
+		//System.out.println(arrayAlg.findMin(values));
+		//System.out.println(arrayAlg.findMaxIndex(values));
+		
+		int [] arrayToShiftRight = {1,2,3,4,5,6,7};
+		
+		int [] shiftedArray1 = arrayAlg.shiftArrayRight(arrayToShiftRight);
+		for(int num:shiftedArray1) {
+			System.out.println(num+" ");
+		}
+		
+		
+		String [] arrayToShiftLeft = {"Apple","Orange","Banana","Grape"};
+		String [] shiftedArray2 = arrayAlg.shiftArrayLeft(arrayToShiftLeft);
+		
+		for(String stuff:shiftedArray2) {
+			System.out.println(stuff+" ");
+		}
 	}
 
 }
--- unit6/src/com/columbushs/unit6/Unit6_4ArrayAlgorithms.java
+++ unit6/src/com/columbushs/unit6/Unit6_4ArrayAlgorithms.java
@@ -1,5 +1,105 @@
 package com.columbushs.unit6;
 
 public class Unit6_4ArrayAlgorithms {
+	
+	//ID max or min value in array
+	
+	public double findMax(double [] arrayIn) {
+		
+		double maxValue = arrayIn[0];
+		
+		for (int i=1;i<arrayIn.length;i++) {
+			
+			if(arrayIn[i]>maxValue) {
+				maxValue = arrayIn[i];
+			}	
+		}		
+		return maxValue;
+	}
+	
+	public double findMin(double [] arrayIn) {
+	
+		double minValue = Double.MAX_VALUE;
+		
+		for (double currentValue:arrayIn) {
+			
+			if(currentValue<minValue) {
+				
+				minValue = currentValue;
+			}
+		}
+		return minValue;
+	}
+	
+	//Find index of max value ???
+	//What do I need to add?
+	
+	public int findMaxIndex(double [] arrayIn) {
+		
+		int maxIndex = 0;
+		double maxValue = arrayIn[0];
+		
+		for (int i=1;i<arrayIn.length;i++) {
+			
+			if(arrayIn[i]>maxValue) {
+				maxValue = arrayIn[i];
+			}	
+		}		
+		return maxIndex;
+	}
+	
+	// Calculate Average
+	
+	public double getArrayAvg(double [] arrayIn) {
+		
+		double average = 0.0;
+		
+		//????
+		
+		return average;
+		
+	}
+	
+	
+	//Shifting Array contents
+	public int[] shiftArrayRight(int [] arrayIn) {
+		
+		int[] shifted = new int[arrayIn.length];
+		int shift = 8;
+		
+		for (int i=0;i<arrayIn.length;i++) {
+			
+			shifted[Math.abs((i+shift)%arrayIn.length)] = arrayIn[i];
+		}
+		
+		return shifted;
+	}
+	
+	public String[] shiftArrayLeft(String [] arrayIn) {
+		
+		String[] shifted = new String[arrayIn.length];
+		int shift = 2;
+		String temp;
+		for(int count=0;count<shift;count++) {
+			
+			if (count<1) {
+				temp = arrayIn[0];
+			}else {
+				temp=shifted[0];
+			}			
+			for (int i=0;i<arrayIn.length-1;i++) {
+			
+				if (count<1){
+					shifted[i] = arrayIn[i+1];
+				}else{
+					shifted[i] = shifted[i+1];
+				}
+			}
+			shifted[arrayIn.length-1]=temp;
+			
+		}
+		return shifted;
+	}
+	
 
 }
